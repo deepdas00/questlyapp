14c13f14f6c3f606d363a48d61029a0b0114a529
Deep Das <itsdeep890@gmail.com>
2026-05-05 21:33:32 +0530

    Install function done

--- attendance-tracker/src/main.jsx
+++ attendance-tracker/src/main.jsx
@@ -6,6 +6,19 @@ import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import { Toaster } from "react-hot-toast";
 
+
+// 🔥 ADD THIS HERE
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("SW registered"))
+      .catch((err) => console.log("SW failed", err));
+  });
+}
+
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
