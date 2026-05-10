b754986784b1ea3e44bd38d94910ef386710219b
Deep Das <itsdeep890@gmail.com>
2026-05-10 09:40:28 +0530

    DONE dashboard not refreshing

--- attendance-tracker/src/context/AppContext.jsx
+++ attendance-tracker/src/context/AppContext.jsx
@@ -3,16 +3,17 @@ import { createContext, useContext, useState } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
+  //attendance page
+  const [attendance, setAttendance] = useState([]);
+  const [subjects, setSubjects] = useState([]);
+  const [routine, setRoutine] = useState([]);
+  const [semester, setSemester] = useState(null);
 
-//attendance page
-const [attendance, setAttendance] = useState([]);
-const [subjects, setSubjects] = useState([]);
-const [routine, setRoutine] = useState([]);
-const [semester, setSemester] = useState(null);
+  //taskify pages
+  const [tasks, setTasks] = useState(null);
 
-
-//taskify pages
-const [tasks, setTasks] = useState(null);
+  //Dashboard page/home page
+  const [dashboardData, setDashboardData] = useState(null);
 
   return (
     <AppContext.Provider
@@ -26,9 +27,12 @@ const [tasks, setTasks] = useState(null);
         semester,
         setSemester,
 
-
         tasks,
         setTasks,
+        
+
+        dashboardData,
+        setDashboardData,
       }}
     >
       {children}
